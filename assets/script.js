de25bfdc1c709e5c6b883b8f5ba983e0ecf758bb
cpkopp <cameronpkopp@gmail.com>
2023-09-21 19:53:45 -1000

    search form

--- assets/script.js
+++ assets/script.js
@@ -49,8 +49,22 @@ const mainEls = document.querySelector("#main-els");
 const mainElsRow = document.createElement("div");
 mainElsRow.setAttribute("class","row");
 mainEls.appendChild(mainElsRow);
+const formEl = document.querySelector("#search");
+const searchInputEl = document.querySelector("#search-name");
+const showAllBtnEl = document.querySelector("#show-all-data-btn");
 
+//Loop through getData() object array and get index coresponding to a given name
+//if no names match search return -1
+var nameSearch = function(dataObject, searchedName) {
 
+    for(let i = 0; i < dataObject.length; i++) {
+        if (searchedName === dataObject[i].name) {
+            return i;
+        } 
+    }
+
+    return -1;
+}
 
 //function to load data card
 var loadDataCard = function(cardIndex) {
@@ -107,7 +121,11 @@ var loadDataCard = function(cardIndex) {
 
 }
 
+//Load all data by looping through list given by getData()
 var loadAllDataCards = function() {
+
+    //Remove current data
+    mainElsRow.replaceChildren();
 
     //loop through cards by index
     for (let i=0; i<dataList.length; i++) {
@@ -116,4 +134,47 @@ var loadAllDataCards = function() {
 
 }
 
+//search form sumbit handler function
+var formSubmitHandler = function(event){
+    event.preventDefault();
+    
+    //get name from input
+    var name = searchInputEl.value;
+    var nameIndex = nameSearch(dataList, name);
+
+    if ( nameIndex != -1){
+        //remove current data
+        mainElsRow.replaceChildren();
+
+        //show searched data
+        loadDataCard(nameIndex);
+    } else {
+        // either alert user name not found or prompt to enter name
+        if (name) {
+            alert('Name not found.');
+        } else {
+            alert('Please enter a name to search.');
+        }
+    }
+
+    //clear search bar
+    searchInputEl.value = '';
+};
+
+
+var showAllClickHandler = function(event) {
+    event.preventDefault();
+
+    //clear current data
+    mainElsRow.replaceChildren();
+
+    //load all data cards
+    loadAllDataCards();
+}
+
+//initially load all data
 loadAllDataCards();
+
+//event listeners, form and show all btn
+formEl.addEventListener("submit", formSubmitHandler);
+showAllBtnEl.addEventListener("click", showAllClickHandler);
